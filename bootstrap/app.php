3f0166b0b8cf5caf6a0ec3ec223c861af9e77138
David <daviddwiyanto79@gmail.com>
2024-10-14 01:41:54 +0700

    fix: deploymeny xx

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             'api/*',
+            'api/',
+            'posts/',
+            'posts/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
