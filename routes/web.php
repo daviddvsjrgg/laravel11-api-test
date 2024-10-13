a3ef2785d4a42a8b3cdd83eacf6fea18c58b417f
David <daviddwiyanto79@gmail.com>
2024-10-14 01:55:23 +0700

    fix: deploymeny xx

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@ Route::post('/posts', [PostController::class, 'store']); // Create a new post
 Route::get('/posts/{post}', [PostController::class, 'show']); // Get a specific post
 Route::put('/posts/{post}', [PostController::class, 'update']); // Update a specific post
 Route::delete('/posts/{post}', [PostController::class, 'destroy']); // Delete a specific post
+
+Route::get('/david-get-storage-link', function() {
+    Artisan::call(storage::link);
+}); // Get all posts
+
