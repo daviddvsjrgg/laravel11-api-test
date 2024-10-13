0eacf49399252f4efea039f05d9f4abc3ee36450
David <daviddwiyanto79@gmail.com>
2024-10-14 00:46:34 +0700

    fix: deploymeny xx

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,14 +6,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/api.php',
+        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Disable CSRF protection for specific routes
         $middleware->validateCsrfTokens(except: [
-            'laravel11-api-test-daviddvsjrggs-projects.vercel.app/api/*',
+            'https://laravel11-api-test-daviddvsjrggs-projects.vercel.app/api/*',
+            'https://laravel11-api-test-daviddvsjrggs-projects.vercel.app/*'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
